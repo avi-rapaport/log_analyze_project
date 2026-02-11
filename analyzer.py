50db92b473bb6f1ac50b739dfe9b1c3603d70c52
Avi Rapaport <kenblock.ar@gmail.com>
2026-02-11 16:17:26 +0200

    filtering by size and tagging

--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,4 @@
-from checks import is_external_ip,is_sensitive_port
+from checks import is_external_ip,is_sensitive_port,is_large_packet
 
 
 def identifying_suspicions_ips(data):
@@ -11,8 +11,13 @@ def identifying_sensitives_ports(data):
     return sensitives_ports
 
 
+def identifying_large_packets(data):
+    large_packets = [packet for packet in data if is_large_packet(packet[5])]
+    return large_packets
 
-
+def tag_large_packets(data):
+    tag_list = [log + ["LARGE"] if is_large_packet(log[5]) else log + ["NORMAL"] for log in data ]
+    return tag_list
 
 
 
--- checks.py
+++ checks.py
@@ -1,7 +1,10 @@
-from config import INTERNAL_IP, SENSITIVE_PORT
+from config import INTERNAL_IP, SENSITIVE_PORT,LARGE_PACKET
 
 def is_external_ip(ip_address):
     return not ip_address.startswith(INTERNAL_IP)
 
 def is_sensitive_port(port):
     return port in SENSITIVE_PORT
+
+def is_large_packet(packet):
+    return packet > LARGE_PACKET
